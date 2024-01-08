9465488f42bb129698eb7d94edfd18a962f3d4c4
Manoj <manojgowdacs35@gmail.com>
2024-01-08 23:17:38 +0530

    vendor routes

---
+++ server/routes/vendor.routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { createVendor, deleteVendor, getVendor, getVendorList, updateVendor } from '../controllers/vendor.js';
+
+const router  = express.Router();
+
+router.post('/create',createVendor)
+router.delete('/delete/:id',deleteVendor)
+router.post('/update/:id',updateVendor)
+router.get('get/:id',getVendor)
+router.get('/get',getVendorList)
+
+export default router;
