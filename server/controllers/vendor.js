2c2ebc023691affa28a4079ef8f2bd42c19c5e17
Manoj <manojgowdacs35@gmail.com>
2024-01-08 23:19:31 +0530

    vendor controller file

---
+++ server/controllers/vendor.js
@@ -0,0 +1,15 @@
+export const createVendor = async (req,res,next)=>{
+    
+}
+export const deleteVendor = async (req,res,next)=>{
+
+}
+export const updateVendor = async (req,res,next)=>{
+
+}
+export const getVendor = async (req,res,next)=>{
+
+}
+export const getVendorList = async (req,res,next)=>{
+
+}
