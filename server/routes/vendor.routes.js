c55a65869638ce76b087510c7c5799b159f309d3
Manoj <manojgowdacs35@gmail.com>
2024-01-09 14:43:52 +0530

    vendor routes

--- server/routes/vendor.routes.js
+++ server/routes/vendor.routes.js
@@ -6,7 +6,7 @@ const router  = express.Router();
 router.post('/create',createVendor)
 router.delete('/delete/:id',deleteVendor)
 router.post('/update/:id',updateVendor)
-router.get('get/:id',getVendor)
+router.get('/get/:id',getVendor)
 router.get('/get',getVendorList)
 
 export default router;
