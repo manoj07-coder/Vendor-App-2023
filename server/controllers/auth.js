a6ef39f1a0c6b1b64428a9c0796b18ba8ba67269
Manoj <manojgowdacs35@gmail.com>
2024-01-09 14:43:21 +0530

    auth file

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js'
+import { errorHandler } from '../utils/errorHandler.js';
+import jwt from 'jsonwebtoken'
 
 
 
@@ -9,13 +11,29 @@ export const signUp = async (req,res,next)=>{
     const newUser = new User({username,email,password:hashedPassword})
     try {
         await newUser.save();
-        res.status(200).json('User created succesfully')
+        res.status(201).json('User created succesfully')
     } catch (error) {
         next(error)
     }
 }
 export const signIn = async (req,res,next)=>{
-    
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found!'))
+        }
+        const validPassword = bcrypt.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'wrong credentials'));
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const {password:pass,...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true})
+        .status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
 }
 export const google = async (req,res,next)=>{
 
